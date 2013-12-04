b6b930433a02e996f4e9995eada9ce1535c65e17
JaguarPhD <hardingalexh@gmail.com>
2013-12-04 08:38:36 -0500

    streamlining commenting

--- wordnik_script.py
+++ wordnik_script.py
@@ -8,18 +8,16 @@ definitions_list=[]
 # takes a string
 def gather_definitions(word):
 
-    # gets definitions and many other things... we're just using the definition and part of speech
+    # gets definitions, see docs for complete
     # useCanonical="True" tries to match a base word (ie: cats -> cat), if no luck ignores
-    # this is a large list from many dictionaries... can be filtered down
+    # add flag for single dictionary?
     definition = wordApi.getDefinitions(word,useCanonical="True",)
 
     #setting up counters used for incrementing
     Y=0
     speech_used=[]
     
-    # for loop that checks if the counter is still within the length of the list, and whether
-    # or not the part of speech has been defined already... This essentially allows us to gather
-    # each part of speech once in priority of first appearance 
+    # for loop that iterates list and parts of speech
     for x in range(len(definition)):
     	
         if Y < (len(definition)-1):
