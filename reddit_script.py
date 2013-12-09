1f2500ebc6da30f12ca952d788604dde17421216
alexharding <hardingalexh@gmail.com>
2013-12-08 20:04:07 -0500

    setting up reddit script

--- reddit_script.py
+++ reddit_script.py
@@ -1,1 +1,7 @@
-# reddit bot
+import praw
+r = praw.Reddit (user_agent='Wordnik_Bot')
+r.login('wordnik_bot', 'wordnik_bot')
+
+while True:
+    subreddit = r.get_subreddit('test')
+    for submission in subreddit.get_hot(limit=10):
