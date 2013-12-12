413538fb4b5a7a18e0bed6164348c4e68d5baeae
alexharding <hardingalexh@gmail.com>
2013-12-11 20:08:22 -0500

    functioning reddit script

--- reddit_script.py
+++ reddit_script.py
@@ -1,7 +1,14 @@
 import praw
+import pprint
 r = praw.Reddit (user_agent='Wordnik_Bot')
 r.login('wordnik_bot', 'wordnik_bot')
 
-while True:
-    subreddit = r.get_subreddit('test')
-    for submission in subreddit.get_hot(limit=10):
+subreddits = r.get_subreddit('test')
+subreddits_comments = subreddits.get_comments()
+
+
+
+
+for submission in subreddits_comments:
+    if "wordnik_bot" in submission.body.lower():
+    	print submission.body
